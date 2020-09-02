ede1cc5ebce7857fcbb34c060f111c8f38118350
bitf14m551 <70544823+bitf14m551@users.noreply.github.com>
2020-09-02 14:02:19 -0500

    Add files via upload

---
+++ .github/workflows/Program.cs
@@ -0,0 +1,32 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sqlConnection = @"Data Source = (DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521)))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = ORCL))); ;user id = xx; Password = xxx";
+           
+            OracleConnection conn = new OracleConnection(sqlConnection);
+            OracleCommand cmd = new OracleCommand("pkg_rebellionrider.proc_superhero ", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+         
+            cmd.Parameters.Add("f_name", "varchar2").Value = "Julia";
+            cmd.Parameters.Add("l_name", "varchar2").Value = "Roberts";
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
